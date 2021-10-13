9cec558c5ccc23b2971d6ec470dc0ede3b598c91
Laura <laurapires@cpejr.com.br>
2021-10-13 08:47:18 -0300

    barra de pesquisa do serviço

--- src/pages/Servicos/Servicos.js
+++ src/pages/Servicos/Servicos.js
@@ -1,7 +1,40 @@
 import React from "react";
+import "./Servicos.css";
+import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
+import { BiSearchAlt } from "react-icons/bi";
+
 
 function Servicos(){
-    return <h2 >Servicos</h2>
+    return (
+    <div className="servico">
+        <Navbar/>
+        <div className="fundo">
+
+            <form className="barraBusca">
+                
+                    <input class="form-control me-2" type="search" placeholder="Pesquisar por profissional" aria-label=" Pesquisar por profissional" className="pesquisa"></input>
+                
+                <div className="searchButton">
+                    <button class="btn btn-outline-success" type="submit">
+                    <BiSearchAlt className="icon"/> Buscar
+                    </button>
+                </div>
+            </form>
+
+            <p className="escrito">Encontre o 
+            <br/>
+            profissional 
+            <br/> 
+            ideal no serviço  
+            <br/>
+            que procura!
+        </p>
+        </div>
+     <Footer/>    
+    </div>  
+         
+    )
 }
 
 export default Servicos;

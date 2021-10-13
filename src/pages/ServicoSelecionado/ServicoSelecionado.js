ea916fc26b7e72422c936d5725ef5d5c30b201e8
Laura <laurapires@cpejr.com.br>
2021-10-13 16:54:15 -0300

    Início profissionais

--- src/pages/ServicoSelecionado/ServicoSelecionado.js
+++ src/pages/ServicoSelecionado/ServicoSelecionado.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
 import "./ServicoSelecionado.css";
+import { Avatar } from "@material-ui/core";
+import { Button } from "react-bootstrap";
 
 function ServicoSelecionado() {
     return (
@@ -10,6 +12,28 @@ function ServicoSelecionado() {
             <h1 className="title">ServicoSelecionado</h1>
             <div className="especificacoes">
                 <p className="description">Especificações</p>
+            </div>
+            <br/>
+            <br/>
+            <div className="profissionais">
+                <div className="perfilLaranja">
+                <Avatar sx={{ width: 150, height: 150, bgcolor: "#FFBF69" }}>
+                    <h1 className="icone">N</h1>
+                </Avatar>
+                <div className="caixa1">
+                    <p>Nome
+                        <br/>
+                        Estado
+                        <br/>
+                        Cidade
+                    </p>
+                </div>
+                </div>
+                <div className="perfilAzul">
+                <Avatar sx={{ width: 150, height: 150, bgcolor: "#5FC2B0" }}>
+          <h1 className="icone">N</h1>
+        </Avatar>
+                </div>
             </div>
             <Footer/>
         </div>

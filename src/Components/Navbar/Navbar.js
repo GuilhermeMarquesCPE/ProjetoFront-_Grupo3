27e8534cdcd07bcc33547e2385813cc7c3a35850
Matheus <matheusbastos@cpejr.com.br>
2021-10-25 22:00:50 -0300

    Responsividade da navbar quase finalizada
    Problema: não conseguimos fazer a funcionalidade do  menu

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,19 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
+
 function Navbar() {
     return (
     <div className="cabecalhoN">
-
+            <div className="LogoButton">
             <img src="/Images/logo.png" alt="logo" className="logoN"></img>
-
+            <button className="buttonNavbar">&#9776;</button>
+            </div>
+                         
             <div className="navbar">
                 <Link to="home" className="navbarLink">Home</Link>
                 <Link to="servicos" className="navbarLink">Serviços</Link>
                 <Link to="perfil" className="navbarLink">Perfil</Link>
                 <Link to="Cadastro" className="navbarLink">Cadastro</Link>
                 <Link to="Login" className="navbarLink">Login</Link>
-            </div>
+            </div>           
 
         </div>
     );

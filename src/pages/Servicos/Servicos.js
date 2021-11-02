41444a4bc19c96dabb881cb4c2b92d29802e9695
Matheus <matheusbastos@cpejr.com.br>
2021-11-02 19:52:58 -0300

    Alterações no serviço e perfil

--- src/pages/Servicos/Servicos.js
+++ src/pages/Servicos/Servicos.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Servicos.css";
 import { Button } from "react-bootstrap";
 import { BiSearchAlt } from "react-icons/bi";
 import Footer from "../../Components/Footer";
 import Navbar from "../../Components/Navbar";
+import api from "../../services/api";
 
 
 function Servicos(){
+
+    const[elementos, setElementos] = useState([]);
+
+    useEffect(async() => {
+        const tipos = await api.get('/servico/tipos');
+        const auxTipos = [];
+        tipos.data.forEach((response) => {
+            auxTipos.push({label:response.nome, value:response.servico_id});
+        });
+        
+        setElementos(auxTipos);
+        
+    },[]);
+    
     const data= [
         "Professores Particulares", "Fotógrafos", "Engenheiros", "Encanadores", "Programadores", "Editores de Vídeo", "Pintores", "Cuidadores de Pet", "Cozinheiros", "Costureiros", "Babás", "Enfermeiros"
     ]
@@ -31,9 +46,9 @@ function Servicos(){
 
                 </div>
                 <div className="trabalhos">
-                    {data.map((element, index) => (
+                    {elementos.map((element, index) => (
                         <>
-                        <Button variant="outline-dark" className="emprego">{element}</Button> 
+                        <Button variant="outline-dark" className="emprego" value={element.value}>{element.label}</Button> 
                     {(index%2) ? (<br/>):<></>}
                         </>
                     ))}
